b7d367b14bf1c5fd82749d73468ffc0b5738b99c
MitrichMD <the.mitrich.himself@gmail.com>
2025-05-18 11:58:05 +0300

    Initial commit

---
+++ app/src/main/java/com/keshakot/toodoo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.keshakot.toodoo;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayoutNotes;
+    private FloatingActionButton floatingActionButtonAddNote;
+
+    private ArrayList<Note> notes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+            Note note = new Note(i, "Note " + i, random.nextInt(3));
+            notes.add(note);
+        }
+    }
+
+    private void initViews() {
+        linearLayoutNotes = findViewById(R.id.linearLayoutNotes);
+        floatingActionButtonAddNote = findViewById(R.id.floatingActionButtonAddNote);
+    }
+
+    private void showNotes() {
+
+    }
+}
